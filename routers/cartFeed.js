c24b4d86ce396016b3d8af79d82f9ed33ffd02b4
Łukasz <lukasz.poczta12@gmail.com>
2023-05-31 14:25:32 +0200

    added decreaseQty middleware

--- routers/cartFeed.js
+++ routers/cartFeed.js
@@ -8,10 +8,12 @@ import {
   getCartProducts,
   removeProduct,
   increaseQty,
+  decreaseQty,
 } from '../controllers/cartFeed.js';
 
 router.put('/addToCart', addToCart);
 router.get('/getCartProducts', getUserId, getCartProducts);
-router.put('/product/:id', getUserId, increaseQty);
+router.put('/product-incQty/:id', getUserId, increaseQty);
+router.put('/product-decQty/:id', getUserId, decreaseQty);
 router.delete('/deleteProduct', isAuth, removeProduct);
 export default router;
